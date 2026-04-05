590d43db7753551efeff711ca7d9ca9419e29cca
Edwin <edwindenisovelers9864@gmail.com>
2026-04-05 15:46:57 +0300

    fix

--- setup_and_train.sh
+++ setup_and_train.sh
@@ -6,6 +6,9 @@ cd comp_arch_coursework
 python3 -m venv venv
 source venv/bin/activate
 
+python -m pip install --upgrade pip
+
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 
 nohup python main.py > train.log 2>&1 &
